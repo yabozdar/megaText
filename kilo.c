c02099d39dd5d5a3b6d53f013581d563fdb6e562
Abdu <pro@bozdar.org>
2024-07-06 23:57:20 +0500

    implemented user input

--- kilo.c
+++ kilo.c
@@ -1,4 +1,10 @@
+// We begin with unistd header file.
+#include <unistd.h>
+
+// This is the main function of the program.
+// To avoid cooked mode, we need raw mode in terminal.
 int main() {
-  
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
