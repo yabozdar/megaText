f7a9b04b132878d9e7a1b2e79ad315c8d91a70e6
Abdu <pro@bozdar.org>
2024-07-08 16:30:25 +0500

    program prints the ascii codes of the user input

--- kilo.c
+++ kilo.c
@@ -1,10 +1,41 @@
 // We begin with unistd header file.
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
-// This is the main function of the program.
+// This restores the original attributes of user terminal.
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 // To avoid cooked mode, we need raw mode in terminal.
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  // We use atexit() to register disable RawMode function.
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  // ICANON turns off canonical mode.
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
+// This is the main function of the program.
 int main() {
+  enableRawMode();
+
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+    
+  }
   return 0;
 }
