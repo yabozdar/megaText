4a77f2a25acaf3e3bdfe425edee1f7c65534988d
Abdujabbar Bozdar <yabozdar@icloud.com>
2024-07-19 20:08:25 +0500

    cursor is down now

--- kilo.c
+++ kilo.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -14,28 +15,38 @@
 
 /*** DATA ***/
 // This restores the original attributes of user terminal.
-struct termios orig_termios;
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
+
 
 /*** TERMINAL ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1[H", 3);
+
   perror(s);
   exit(1);
 }
 
 void disableRawMode() {
   // tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
   
 }
 // To avoid cooked mode, we need raw mode in terminal.
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
   // We use atexit() to register disable RawMode function.
   atexit(disableRawMode);
   
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   // IXON turns off ctrl-s and ctrl-q commands to terminal.
   // ICRNL fixes ctrl-m and carraige return issues
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -61,9 +72,52 @@ char editorReadkey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  printf("\r\n");
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1) {
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
+  
+  editorReadkey();
+
+  return -1;
+}
+
+int getWindowSize (int *rows, int *cols) {
+  struct winsize ws;
+
+  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows, cols);
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** OUTPUT ***/
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** INPUT ***/
@@ -73,6 +127,8 @@ void editorProcessKeypress() {
 
   switch (c) {
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
   }
@@ -80,10 +136,17 @@ void editorProcessKeypress() {
 
 /*** INIT ***/
 // This is the main function of the program.
+void initEditor() {
+  if (getWindowSize (&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+  
+}
+
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   return 0;
